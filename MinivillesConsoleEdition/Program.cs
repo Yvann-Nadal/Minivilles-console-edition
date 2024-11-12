fe4e8116b968c8c8a72328521538214d67d66d8b
Furydrack <marius.boulandet@gmail.com>
2024-11-12 14:09:05 +0100

    set up push

--- MinivillesConsoleEdition/Program.cs
+++ MinivillesConsoleEdition/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MinivillesConsoleEdition;
+// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+Piles pile = new Piles();
+foreach(Card c in pile.DrawPile.Keys)
+    Console.WriteLine(c);
